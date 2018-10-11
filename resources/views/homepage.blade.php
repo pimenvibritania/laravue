66ca1c49e9f49f9170daf846d669f9c77297f74a
pimenvibritania <blackbrit13@gmail.com>
2018-10-11 13:56:10 +0700

    initial

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron jumbotron-fluid">
+   <div class="container" id="jumbotron_index">
+     <h1>Bootstrap Tutorial</h1> 
+     <p>Bootstrap is the most popular HTML, CSS...</p> 
+   </div>
+ </div>
+@endsection
